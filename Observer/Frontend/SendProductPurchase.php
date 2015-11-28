8e18cf010b4f3ac3dd62cff705be5821b6724617
Steven Richardson <srichardson@sessiondigital.com>
2015-11-28 09:31:03 +0000

    Add sales observer

--- Observer/Frontend/SendProductPurchase.php
+++ Observer/Frontend/SendProductPurchase.php
@@ -7,19 +7,17 @@ use \Magento\Framework\Event\ObserverInterface;
 use \Richdynamix\PersonalisedProducts\Helper\Config;
 use \Magento\Customer\Model\Session as CustomerSession;
 use \Richdynamix\PersonalisedProducts\Model\PredictionIO\Factory;
-use \Richdynamix\PersonalisedProducts\Model\Frontend\GuestCustomers;
 
 /**
- * Listen for product view events and log the customer actions in the PredictionIO
- * server if the customer is logged in. If the customer is a guest then we record the
- * product views in the session to process later.
+ * Listen for sales order event and record the customer-buy-product action in
+ * PredictionIO.
  *
  * @category    Richdynamix
  * @package     PersonalisedProducts
  * @author 		Steven Richardson (steven@richdynamix.com) @mage_gizmo
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class SendProductViews implements ObserverInterface
+class SendProductPurchase implements ObserverInterface
 {
     /**
      * @var Config
@@ -37,56 +35,48 @@ class SendProductViews implements ObserverInterface
     protected $_predictionIOFactory;
 
     /**
-     * @var GuestCustomers
-     */
-    protected $_guestCustomers;
-
-    /**
      * SendProductViews constructor.
      * @param Config $config
      * @param CustomerSession $customerSession
      * @param Factory $predictionIOFactory
-     * @param GuestCustomers $guestCustomers
      */
     public function __construct(
         Config $config,
         CustomerSession $customerSession,
-        Factory $predictionIOFactory,
-        GuestCustomers $guestCustomers
+        Factory $predictionIOFactory
     )
     {
         $this->_config = $config;
         $this->_customerSession = $customerSession;
         $this->_predictionIOFactory = $predictionIOFactory;
-        $this->_guestCustomers = $guestCustomers;
     }
 
-    /**
-     * @param Observer $observer
-     * @return null
-     */
     public function execute(Observer $observer)
     {
         if (!$this->_config->isEnabled()) {
             return;
         }
 
-        $product = $observer->getProduct();
+        $order = $observer->getOrder();
+        $productCollection = $order->getItemsCollection();
 
         if ($this->_customerSession->isLoggedIn()) {
+            $this->_sendPurchaseEvent($productCollection);
+            return;
+        }
+    }
+
+    private function _sendPurchaseEvent($productCollection)
+    {
+        foreach ($productCollection as $product) {
             $eventServer = $this->_predictionIOFactory->create('event');
             $eventServer->createEvent(array(
-                'event' => 'view',
+                'event' => 'buy',
                 'entityType' => 'user',
                 'entityId' => $this->_customerSession->getCustomerId(),
                 'targetEntityType' => 'item',
                 'targetEntityId' => $product->getId()
             ));
-
-            return;
         }
-
-        $this->_guestCustomers->setGuestCustomerProductView($product);
-
     }
 }
