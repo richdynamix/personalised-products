0dd3daa29429f221439de71831b8cec7ea3c77f2
Steven Richardson <srichardson@sessiondigital.com>
2015-12-01 10:32:53 +0000

    Add first test and check with CI

---
+++ Tests/Block/Product/ProductList/UpsellTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sarichardson
+ * Date: 01/12/2015
+ * Time: 10:29
+ */
+
+namespace Richdynamix\PersonalisedProducts\Tests\Block\Product\ProductList;
+
+
+class UpsellTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
